673ca41afdf9c902026714bc959cdbddf2daaa61
Felipe Faura <felipefaura@gmail.com>
2020-12-04 03:05:18 +0100

    production variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBLUy27luGNg_vs9o4Qw1HqzuAqRQePHMU",
+    authDomain: "invisiblefriend-fcbe4.firebaseapp.com",
+    projectId: "invisiblefriend-fcbe4",
+    storageBucket: "invisiblefriend-fcbe4.appspot.com",
+    messagingSenderId: "286563631036",
+    appId: "1:286563631036:web:8f64c7fa595ebaa5a8f5b9"
+  }
 };
